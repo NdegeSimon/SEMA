c4e3a89e43b8afdabd8019f4befddfbf9398562c
Simon Ndege <simopetley@gmail.com>
2025-07-07 16:53:59 +0300

    add changes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const menuIcon = document.getElementById('menu-icon');
-const navMenu = document.getElementById('nav-menu');
+const menuIcon = document.getElementById("menu-icon");
+const navMenu = document.getElementById("nav-menu");
 
-menuIcon.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
-
-  const icon = menuIcon.querySelector('i');
-  icon.classList.toggle('fa-bars');
-  icon.classList.toggle('fa-times');
+menuIcon.addEventListener("click", function() {
+  navMenu.classList.toggle("show"); // toggle the visibility with a new class
 });
+
+ const textarea = document.querySelector("textarea");
+
+  textarea.addEventListener("input", () => {
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + "px";
+     });
